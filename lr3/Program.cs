ebdd7adb72b004b814d96287fde021ab2ac7150c
Bogdan <HitmasterGames006@gmail.com>
2024-06-14 01:51:51 +0300

    Добавлена формула расчёта периметра #4

--- lr3/Program.cs
+++ lr3/Program.cs
@@ -6,7 +6,7 @@ namespace lr3
     {
         static void Main(string[] args)
         {
-            double a, b, Rad, A, H, B;
+            double a, b, Rad, A, H, B, C, D;
             int variant;
 
             Console.WriteLine("1. Прямоугольник");
@@ -34,6 +34,7 @@ namespace lr3
                         temp = Console.ReadLine();
                         b = Convert.ToDouble(temp);
                         Console.WriteLine("Площадь прямоугольника = " + a * b);
+                        Console.WriteLine("Периметр прямоугольника = " + (a + b)*2);
                         break;
 
                     case 2:
@@ -41,16 +42,24 @@ namespace lr3
                         temp = Console.ReadLine();
                         Rad = Convert.ToDouble(temp);
                         Console.WriteLine("Площадь круга = " + 4 * Math.Atan(1.0) * Math.Pow(Rad, 2.0));
+                        Console.WriteLine("Периметр круга = " + (Math.PI*Rad) * 2);
                         break;
                     case 3:
                         Console.Write("Основание A = ");
                         temp = Console.ReadLine();
-                        A = Convert.ToDouble(temp); 
+                        A = Convert.ToDouble(temp);
+                        Console.Write("Сторона B = ");
+                        temp = Console.ReadLine();
+                        B = Convert.ToDouble(temp);
+                        Console.Write("Сторона C = ");
+                        temp = Console.ReadLine();
+                        C = Convert.ToDouble(temp);
                         Console.Write("Высота H = ");
                         temp = Console.ReadLine();
                         H = Convert.ToDouble(temp);
                         Console.WriteLine("Площадь треугольника = "+(A*H)/2);
-                        Console.ReadKey();
+                        Console.WriteLine("Периметр треугольника = " + (A + B + C));
+                        Console.ReadKey(); 
                         break;
                     case 4:
                         Console.Write("Основание A = ");
@@ -59,10 +68,17 @@ namespace lr3
                         Console.Write("Основание B = ");
                         temp = Console.ReadLine();
                         B = Convert.ToDouble(temp);
+                        Console.Write("Сторона C = ");
+                        temp = Console.ReadLine();
+                        C = Convert.ToDouble(temp);
+                        Console.Write("Сторона D = ");
+                        temp = Console.ReadLine();
+                        D = Convert.ToDouble(temp);
                         Console.Write("Высота H = ");
                         temp = Console.ReadLine();
                         H = Convert.ToDouble(temp);
-                        Console.WriteLine("Площадь треугольника = " + (A + B) * H / 2);
+                        Console.WriteLine("Площадь трапеции = " + (A + B) * H / 2);
+                        Console.WriteLine("Периметр трапеции = " + (A + B + C + D));
                         Console.ReadKey();
                         break;
 
